60d33ee9a0c3fe85fc4ec5fae81143d0b23a633a
Andre Okoh <andrefresh0@gmail.com>
2024-05-20 12:36:07 +0100

    update commit

--- src/main/java/com/example/todolistapp/ImplTodoList.java
+++ src/main/java/com/example/todolistapp/ImplTodoList.java
@@ -13,8 +13,7 @@ import static javafx.geometry.Pos.*;
 
 public class ImplTodoList implements TodoList {
     private Connection connection;
-
-    // main
+    
 
     public ImplTodoList(Connection connection) {
         this.connection = connection;
